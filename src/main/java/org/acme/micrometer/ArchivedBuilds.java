cb1fbd942e6004721e55595c05f3faa1b15b9574
Leonid Bossis <lbossis@redhat.com>
2021-04-01 10:28:12 -0400

    removed hibernate.cfg.xml, created mapping for _archived_buildrecords table

---
+++ src/main/java/org/acme/micrometer/ArchivedBuilds.java
@@ -0,0 +1,209 @@
+package org.acme.micrometer;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name="_archived_buildrecords")
+@Access(value=AccessType.FIELD)
+public class ArchivedBuilds {
+
+    @Id
+    @Column(name = "buildrecord_id", unique = true, nullable = false)
+    private int buildRecordId;
+
+    @Column(name = "submittime")
+    private Timestamp submitTime;
+
+    @Column(name = "starttime")
+    private Timestamp startTime;
+
+    @Column(name = "endtime")
+    private Timestamp endTime;
+
+    @Column(name = "submit_year")
+    private int submitYear;
+
+    @Column(name = "submit_month")
+    private int submitMonth;
+
+    @Column(name = "submit_quarter")
+    private int submitQuarter;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "buildtype")
+    private String buildType;
+
+    @Column(name = "executionrootname")
+    private String executionRootName;
+
+    @Column(name = "executionrootversion")
+    private String executionRootVersion;
+
+    @Column(name = "user_id", nullable = false)
+    private int userId;
+
+    @Column(name = "temporarybuild")
+    private boolean temporaryBuild;
+
+    @Column(name = "autoalign")
+    private boolean autoAlign;
+
+    @Column(name = "trimmed_buildlog")
+    private String trimmedBuildLog;
+
+    @Column(name = "trimmed_repourlog")
+    private String trimmedRepourLog;
+
+    @Column(name = "categorized_error_msg")
+    private String categorizedErrorMsg;
+
+    @Column(name = "categorized_error_group")
+    private String categorizedErrorGroup;
+
+    public int getBuildRecordId() {
+        return buildRecordId;
+    }
+
+    public void setBuildRecordId(int buildRecordId) {
+        this.buildRecordId = buildRecordId;
+    }
+
+    public Timestamp getSubmitTime() {
+        return submitTime;
+    }
+
+    public void setSubmitTime(Timestamp submitTime) {
+        this.submitTime = submitTime;
+    }
+
+    public Timestamp getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Timestamp startTime) {
+        this.startTime = startTime;
+    }
+
+    public Timestamp getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Timestamp endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getSubmitYear() {
+        return submitYear;
+    }
+
+    public void setSubmitYear(int submitYear) {
+        this.submitYear = submitYear;
+    }
+
+    public int getSubmitMonth() {
+        return submitMonth;
+    }
+
+    public void setSubmitMonth(int submitMonth) {
+        this.submitMonth = submitMonth;
+    }
+
+    public int getSubmitQuarter() {
+        return submitQuarter;
+    }
+
+    public void setSubmitQuarter(int submitQuarter) {
+        this.submitQuarter = submitQuarter;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getBuildType() {
+        return buildType;
+    }
+
+    public void setBuildType(String buildType) {
+        this.buildType = buildType;
+    }
+
+    public String getExecutionRootName() {
+        return executionRootName;
+    }
+
+    public void setExecutionRootName(String executionRootName) {
+        this.executionRootName = executionRootName;
+    }
+
+    public String getExecutionRootVersion() {
+        return executionRootVersion;
+    }
+
+    public void setExecutionRootVersion(String executionRootVersion) {
+        this.executionRootVersion = executionRootVersion;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public boolean isTemporaryBuild() {
+        return temporaryBuild;
+    }
+
+    public void setTemporaryBuild(boolean temporaryBuild) {
+        this.temporaryBuild = temporaryBuild;
+    }
+
+    public boolean isAutoAlign() {
+        return autoAlign;
+    }
+
+    public void setAutoAlign(boolean autoAlign) {
+        this.autoAlign = autoAlign;
+    }
+
+    public String getTrimmedBuildLog() {
+        return trimmedBuildLog;
+    }
+
+    public void setTrimmedBuildLog(String trimmedBuildLog) {
+        this.trimmedBuildLog = trimmedBuildLog;
+    }
+
+    public String getTrimmedRepourLog() {
+        return trimmedRepourLog;
+    }
+
+    public void setTrimmedRepourLog(String trimmedRepourLog) {
+        this.trimmedRepourLog = trimmedRepourLog;
+    }
+
+    public String getCategorizedErrorMsg() {
+        return categorizedErrorMsg;
+    }
+
+    public void setCategorizedErrorMsg(String categorizedErrorMsg) {
+        this.categorizedErrorMsg = categorizedErrorMsg;
+    }
+
+    public String getCategorizedErrorGroup() {
+        return categorizedErrorGroup;
+    }
+
+    public void setCategorizedErrorGroup(String categorizedErrorGroup) {
+        this.categorizedErrorGroup = categorizedErrorGroup;
+    }
+}
--- src/main/java/org/acme/micrometer/DbCnxnViaHibernate.java
+++ src/main/java/org/acme/micrometer/DbCnxnViaHibernate.java
@@ -2,15 +2,14 @@ package org.acme.micrometer;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.query.Query;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -18,10 +17,14 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/")
+@ApplicationScoped
 public class DbCnxnViaHibernate {
     private static final String className = DbCnxnViaHibernate.class.getName();
 
-    @Inject @ApplicationScoped
+    @Inject
+    EntityManagerFactory emFactory;
+
+    @Inject
     MeterRegistry registry;
 
     private Counter totalBuildsCounter;
@@ -33,36 +36,10 @@ public class DbCnxnViaHibernate {
     }
 
     private int getLatestTotalCountOfBuilds() {
-        Integer count = 0;
-        Session session = null;
-        try {
-            SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-            if (sessionFactory == null) {
-                System.out.println("buildSessionFactory() returned null");
-                return -1;
-            }
-            session = sessionFactory.openSession();
-            if (session == null) {
-                System.out.println("sessionFactory.openSession() returned null");
-                return -1;
-            }
-            session.beginTransaction();
-            String qry = "SELECT count(*) FROM _archived_buildrecords WHERE temporarybuild == FALSE";
-            Query countQry = session.createQuery(qry);
-            List<Integer> rs = countQry.getResultList();
-            count = rs.get(0);
-            System.out.println("Records count = " + count);
-            session.getTransaction().commit();
-        } catch (HibernateException e) {
-            System.out.println("Failed to create sessionFactory object - " + e.getCause());
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
-        return count;
+        EntityManager entityManager = emFactory.createEntityManager();
+        String qry = "SELECT count(*) FROM _archived_buildrecords WHERE temporarybuild = False";
+        List rs = entityManager.createQuery(qry).getResultList();
+        return (int) rs.get(0);
     }
 
     @GET
